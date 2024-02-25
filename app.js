1c02bb330bd91549157b84d81a315d035d66258e
Sayed Atif <atif.ghafaar@gmail.com>
2024-02-25 15:06:17 -0500

    All Files Added

---
+++ app.js
@@ -0,0 +1,9 @@
+//JS Function to change text color
+function changeColor(){
+    document.getElementById('myName').style.color="#7fffd4";
+    document.getElementById('Software').style.color="blue";
+}
+
+function changeColor2(){
+    document.getElementByClassName('container').style.color="#7fffd4";
+}
